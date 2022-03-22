ee51e2fa4c60b2c37edbafa648145071efded0f4
1bro23 <shinlia21@gmail.com>
2022-03-23 04:30:36 +0800

    collaboration implemented

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -7,6 +7,7 @@ class UserHandlers{
 
         this.postUserHandler = this.postUserHandler.bind(this);
         this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+        this.getUsersByUsernameHandler = this.getUsersByUsernameHandler.bind(this);
     }
     async postUserHandler(request,h){
         try{
@@ -68,6 +69,36 @@ class UserHandlers{
             console.log(error(error));
             return response;
         }
+    }
+    async getUsersByUsernameHandler(request, h) {
+        try {
+            const { username = '' } = request.query;
+            const users = await this._service.getUsersByUsername(username);
+            return {
+              status: 'success',
+              data: {
+                users,
+              },
+            };
+          } catch (error) {
+            if (error instanceof ClientError) {
+              const response = h.response({
+                status: 'fail',
+                message: error.message,
+              });
+              response.code(error.statusCode);
+              return response;
+            }
+       
+            // Server ERROR!
+            const response = h.response({
+              status: 'error',
+              message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+          }
     }
 }
 module.exports=UserHandlers;
--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -7,6 +7,11 @@ const routes = (handler)=>[
         method:"GET",
         path:"/users/{id}",
         handler:handler.getUserByIdHandler
-    }
+    },
+    {
+        method: 'GET',
+        path: '/users',
+        handler: handler.getUsersByUsernameHandler,
+    },
 ]
 module.exports=routes;
