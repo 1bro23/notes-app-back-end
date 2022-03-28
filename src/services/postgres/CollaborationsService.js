03bf1bd97edbc56cb6611c693bd01834d8f5701f
1bro23 <shinlia21@gmail.com>
2022-03-28 12:29:51 +0800

    server-side caching implemented

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -2,8 +2,9 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
  
 class CollaborationsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+    this._cacheService = cacheService;
   }
   async addCollaboration(noteId, userId) {
     const id = `collab-${nanoid(16)}`;
@@ -16,6 +17,7 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
+    await this._cacheService.delete(`notes:${userId}`);
     return result.rows[0].id;
 }
 async deleteCollaboration(noteId, userId) {
@@ -29,6 +31,7 @@ async deleteCollaboration(noteId, userId) {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal dihapus');
     }
+    await this._cacheService.delete(`notes:${userId}`);
   }
   async verifyCollaborator(noteId, userId) {
     const query = {
